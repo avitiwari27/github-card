c6d82ec4752f8e263c04bff7e7e1cfce3fa02aa9
Avi tiwari <avithegr807@gmail.com>
2020-04-02 17:27:21 +0545

    Updates

---
+++ precache-manifest.6cb94078f4d6bcff6116d0edc3d74b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a0c0e227a6da7d4764c7d544ed3c1c",
+    "url": "/todos-react/public/index.html"
+  },
+  {
+    "revision": "83d57e487249c95422aa",
+    "url": "/todos-react/public/static/css/main.64b180fc.chunk.css"
+  },
+  {
+    "revision": "b7d9d3ecdcf78099f218",
+    "url": "/todos-react/public/static/js/2.5a0a65c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-react/public/static/js/2.5a0a65c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d57e487249c95422aa",
+    "url": "/todos-react/public/static/js/main.76ab7977.chunk.js"
+  },
+  {
+    "revision": "63343f1c5662edde4403",
+    "url": "/todos-react/public/static/js/runtime-main.e3deb033.js"
+  }
+]);
